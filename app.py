08978d5b0b049d6db59b14d179daacc2dd35374d
Kailun Hsu <t23795@gmail.com>
2022-03-31 16:11:38 +0800

    完成 booking api

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import *
 # 景點 api 藍圖
 from route.attractionAPI import attractionAPI
 from route.memberAPI import memberAPI
+from route.bookingAPI import bookingAPI
+
 app=Flask(__name__)
 app.config["JSON_AS_ASCII"]=False
 app.config["TEMPLATES_AUTO_RELOAD"]=True
@@ -9,6 +11,7 @@ app.secret_key= "1234567890"
 
 app.register_blueprint(attractionAPI)
 app.register_blueprint(memberAPI)
+app.register_blueprint(bookingAPI)
 
 # Pages
 @app.route("/")
